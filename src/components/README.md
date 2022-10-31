9103af75f4549d3cc500d23c7ab42cc1d9a56ec6
Hiep Do <hiep23022002@gmail.com>
2022-10-31 12:02:55 +0700

    highligh-job

---
+++ src/components/README.md
@@ -0,0 +1,11 @@
+## sử dụng components public
+
+### HighlightJob trong folder highlight-job
+
+Chuyển tất cả :
+(src/asses/readme/pre-highlight-job.png)
+sang
+(src/asses/readme/highlight-job.png)
+
+Các tham số đầu vào:
+(src/asses/readme/argument-of-highlight-job)
--- src/components/highlight-job/HighlightJob.js
+++ src/components/highlight-job/HighlightJob.js
@@ -1,31 +1,42 @@
 import classes from "./HighlightJob.module.scss"
 import { Link } from "react-router-dom"
+import FollowIcon from "../icon/follow"
 
 const HighlightJob = (props) => {
     return (    
             <div className={`${classes.card} ${props.class}`}>
-                <Link>
                 <div className={classes.company}>
-                    <div className={classes.logo}>
-                        <img src={props.logo} alt=".." />
-                    </div>
-                    <div className={classes.info}>
-                        <h3>{props.companyName}</h3>
-                        <p>{props.location}</p>
+                    <Link>
+                        <div className={classes['company-info']}>
+                            <div className={classes.logo}>
+                                <img src={props.logo} alt=".." />
+                            </div>
+                            <div className={classes.info}>
+                                <h3>{props.category}</h3>
+                                <p>{props.companyName} - {props.location}</p>
+                            </div>
+                        </div>
+                    </Link>
+                    <div className={classes.btn}>
+                        <button>
+                            <FollowIcon />
+                        </button>
                     </div>
                 </div>
-                <div className={classes.job}>
+                <div className={classes.jd}>
                     <ul>
-                        <li className={classes.name}>{props.jobName}</li>
-                        <li>{props.type}</li>
-                        <li>{props.submittedDate}</li>
+                        <li>{props.experience}, {props.jobType}</li>
+                        <li>${props.minSalary} - ${props.maxSalary}</li>
                     </ul>
                 </div>
-                </Link>
-                <div className={classes.button}>
-                    <Link className={classes.btn}>
-                        Apply now
-                    </Link>
+                <div className={classes.skill}>
+                    <ul className={classes['skill-items']}>
+                        {props.skills.map(item => (
+                            <div key={item} className = {classes.item}>
+                                {item}
+                            </div>
+                        ))}
+                    </ul>
                 </div>
             </div>
     )
