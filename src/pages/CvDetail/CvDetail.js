7891f68d27063e9f86283c646ff47844cea608d9
Hiep Do <hiep23022002@gmail.com>
2022-11-04 12:04:33 +0700

    CV details page v2

--- src/pages/CvDetail/CvDetail.js
+++ src/pages/CvDetail/CvDetail.js
@@ -2,7 +2,9 @@ import classes from "./CvDetail.module.scss"
 import locationImg from "../../asses/img-location.png"
 import { Fragment, useState } from "react"
 
-const CvDetail = () => {
+const CvDetail = (props) => {
+
+    const CV = props.cv
 
     const [isEdit, setIsEdit] = useState(false)
 
@@ -23,8 +25,12 @@ const CvDetail = () => {
                 {/* <button>Save</button> */}
                 <button>Save</button>
             </div>
-            {!isEdit && <p>CV's name</p>}
-            {isEdit && <input type="text" />}
+            <div className={classes.name}>
+                <div>
+                    {!isEdit && <p>{CV.name}</p>}
+                    {isEdit && <input type="text" />}
+                </div>
+            </div>
             <div className={classes.cv}>
                 <div className={classes['info1']}>
                     <div className={classes.first}>
@@ -32,15 +38,36 @@ const CvDetail = () => {
                             <img src={locationImg} alt="..."/>
                         </div>
                         <p>Name</p>
-                        <p>Job</p>
+                        <div className={classes['name-user']}>
+                            <div>
+                                {!isEdit && <p>{CV.nameUser}</p>}
+                                {isEdit && <input type="text" />}
+                            </div>
+                        </div>
                     </div>
-                    <div className={classes.second}>
-                        <h2>Experience</h2>
-
+                    <div className={classes.experience}>      
+                        <h2>
+                            Experience
+                        </h2>
+                        <div className={classes['exprience-detail']}>
+                            {CV.experience.map(item => (
+                                <div>
+                                    {!isEdit && <p>{item.startDate} - {item.endDate}</p>}
+                                    {isEdit && <input type="text" />}
+                                    {!isEdit && <h3>{item.position} - {item.company}</h3>}
+                                    {isEdit && <input type="text" />}
+                                    {!isEdit && <p>{item.description}</p>}
+                                    {isEdit && <input type="text" />}
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
                 <div className={classes['info2']}>
-
+                    <div className={classes['own-info']}>
+                        <label>Date of birth:</label>
+                        <p>xx/xx/xxxx</p>
+                    </div>
                 </div>
             </div>
         </form>
---
+++ src/pages/CvDetail/CvDetailPage.js
@@ -0,0 +1,71 @@
+import CvDetail from "./CvDetail"
+
+const DUMMYCV = {
+    id: '1',
+    name: "Cv's Name",
+    nameUser: "Job",
+    birthday: '23/02/2002',
+    phone: '0123456789',
+    email: 'hiepxxxxxx@gmail.com',
+    website: 'none',
+    address: '109xxxxxx',
+    sex: 'male',
+    experience: [
+        {
+            id: '1',
+            company: 'Google',
+            startDate: '10/10/2000',
+            endDate: '10/11/2001',
+            position: 'Manager',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi est sit amet facilisis. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant.',
+        },
+        {
+            id: '2',
+            company: 'Google',
+            startDate: '10/10/2000',
+            endDate: '10/11/2001',
+            position: 'manager',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi est sit amet facilisis. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant.',
+        }
+    ],
+    activities: [
+        {
+            id: '1',
+            organization: 'Hiepdx',
+            startDate: '10/10/2000',
+            endDate: '10/11/2001',
+            position: 'manager',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi est sit amet facilisis. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant.',
+        },
+    ],
+    education: [
+        {
+            id: '1',
+            school: 'Hiepdx',
+            startDate: '10/10/2000',
+            endDate: '10/11/2001',
+            position: 'manager',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi est sit amet facilisis. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant.',
+        },
+    ],
+    skills: [
+        {
+            id: '1',
+            skill: 'cloud',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi est sit amet facilisis. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant.',
+        },
+        {
+            id: '2',
+            skill: 'cloud',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi est sit amet facilisis. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant.',
+        },
+    ]
+}
+
+const CvDetailPage = () => {
+    return (
+        <CvDetail cv = {DUMMYCV}/>
+    )
+}
+
+export default CvDetailPage
