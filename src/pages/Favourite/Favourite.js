b59523a3e895508374c5a05540ccf79a368e4d9e
Hiep Do <hiep23022002@gmail.com>
2022-11-08 15:18:39 +0700

    api v1

--- src/pages/Favourite/Favourite.js
+++ src/pages/Favourite/Favourite.js
@@ -1,6 +1,9 @@
 import { Fragment } from "react"
 import FavouriteJob from "./Components/FavouriteJob"
 import locationImg from "../../asses/img-location.png"
+import { getAllUser } from "../../store/apiRequest";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const DUMMY_JOB = [{
     id: '1',
@@ -23,9 +26,19 @@ const DUMMY_JOB = [{
 ]
 
 const Favourite = () => {
+
+    const dispatch = useDispatch()
+    const allUser = useSelector(state => state.user)
+
+    const getUserHandler = async () => {
+        await getAllUser(dispatch)
+        console.log(allUser)
+    } 
+
     return (
         <Fragment>
             <FavouriteJob jobs = {DUMMY_JOB}/>
+            <button onClick={getUserHandler}>a</button>
         </Fragment>
     )
 }
