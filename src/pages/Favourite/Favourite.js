fa27e0879ab1fc276a98ec901d4b5497f69d01a1
Hiep Do <hiep23022002@gmail.com>
2022-11-14 16:16:31 +0700

    favourite feature v1

--- src/pages/Favourite/Favourite.js
+++ src/pages/Favourite/Favourite.js
@@ -2,8 +2,6 @@ import { Fragment } from "react"
 import FavouriteJob from "./Components/FavouriteJob"
 import locationImg from "../../asses/img-location.png"
 import { getAllUser } from "../../store/apiRequest";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
 
 const DUMMY_JOB = [{
     id: '1',
@@ -11,34 +9,22 @@ const DUMMY_JOB = [{
     companyName: 'Google Inc',
     location: 'California',
     category: 'UI/UX Desgin',
-    type: 'Part Time',
-    submittedDate: '12/12/2012',
+    jobType: 'Part Time',
+    skills: ['a', 'b'],
+    experience: 'efsdfdfs',
+    // minSalary: '1000',
+    // maxSalary: '2000',
+    salary: 1000,
 },
-{
-    id: '2',
-    logo: locationImg,
-    companyName: 'AWS',
-    location: 'Hanoi',
-    category: 'System Engineer',
-    type: 'Full Time',
-    submittedDate: '12/12/2012',
-},
+
 ]
 
 const Favourite = () => {
 
-    const dispatch = useDispatch()
-    const allUser = useSelector(state => state.user)
-
-    const getUserHandler = async () => {
-        await getAllUser(dispatch)
-        console.log(allUser)
-    } 
-
     return (
         <Fragment>
             <FavouriteJob jobs = {DUMMY_JOB}/>
-            <button onClick={getUserHandler}>a</button>
+            {/* <RecomendedJob /> */}
         </Fragment>
     )
 }
