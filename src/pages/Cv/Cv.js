1268d45a9aa14426e592f9074c067686eec155b4
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-16 08:37:22 +0700

    fix: CV page

--- src/pages/Cv/Cv.js
+++ src/pages/Cv/Cv.js
@@ -1,42 +1,50 @@
 import CVPage from "./Component/CV-page";
-import address from '../../asses/address.png';
-import google from '../../asses/google.png'
+import address from "../../asses/address.png";
+import google from "../../asses/google.png";
 const Cv = () => {
-    const ITEMS = [
+  const CVS = [
+    {
+      id: 1,
+      title: "CV 1",
+      lastUpdate: "2 days ago",
+    },
+  ];
+  const ITEMS = [
+    {
+      id: "1",
+      companyName: "NatWest. Group",
+      logo: address,
+      jobs: [
         {
-          id: '1',
-          companyName: 'NatWest. Group',
-          logo: address,
-          jobs: [{
-              id: '1',
-              category: 'Enterprise Architect',
-              jobType: 'London, UK',
-              location: 'NatWest. Group',
-          }, {
-              id: '2',
-              category: 'Privacy Officer',
-              jobType: 'London, UK',
-              location: 'NatWest. Group',
-          }]
-      }
-      ]
-    const JOBS = [
+          id: "1",
+          category: "Enterprise Architect",
+          jobType: "London, UK",
+          location: "NatWest. Group",
+        },
         {
-            id: 1,
-            logo: google,
-            companyName: 'Google Inc',
-            location: 'California',
-            category: 'UI/UX Desgin',
-            jobType: 'Part Time',
-            experience: 'Senior',
-            minSalary: '700',
-            maxSalary: '1500',
-            skills: ['cloud', 'react'],
-            }
-    ]
-    return (
-        <CVPage items={ITEMS} jobs={JOBS}/>
-    )
-}
+          id: "2",
+          category: "Privacy Officer",
+          jobType: "London, UK",
+          location: "NatWest. Group",
+        },
+      ],
+    },
+  ];
+  const JOBS = [
+    {
+      id: 1,
+      logo: google,
+      companyName: "Google Inc",
+      location: "California",
+      category: "UI/UX Desgin",
+      jobType: "Part Time",
+      experience: "Senior",
+      minSalary: "700",
+      maxSalary: "1500",
+      skills: ["cloud", "react"],
+    },
+  ];
+  return <CVPage cvs={CVS} items={ITEMS} jobs={JOBS} />;
+};
 
-export default Cv
+export default Cv;
--- src/pages/Favourite/Favourite.js
+++ src/pages/Favourite/Favourite.js
@@ -1,104 +1,107 @@
-import { Fragment } from "react"
-import FavouriteJob from "./Components/FavouriteJob"
-import locationImg from "../../asses/img-location.png"
-import { getAllUser } from "../../store/apiRequest";
-import { useSelector } from "react-redux"
-import config from "../../config"
-import axios from "axios"
+import { Fragment } from "react";
+import FavouriteJob from "./Components/FavouriteJob";
+import locationImg from "../../asses/img-location.png";
+import { useSelector } from "react-redux";
+import config from "../../config";
+import axios from "axios";
 import { useState, useEffect } from "react";
 
-const DUMMY_JOB = [{
-    id: '1',
+const DUMMY_JOB = [
+  {
+    id: "1",
     logo: locationImg,
-    companyName: 'Google Inc',
-    location: 'California',
-    category: 'UI/UX Desgin',
-    jobType: 'Part Time',
-    skills: ['a', 'b'],
-    experience: 'efsdfdfs',
+    companyName: "Google Inc",
+    location: "California",
+    category: "UI/UX Desgin",
+    jobType: "Part Time",
+    skills: ["a", "b"],
+    experience: "efsdfdfs",
     // minSalary: '1000',
     // maxSalary: '2000',
     salary: 1000,
-},
-
-]
+  },
+];
 
 const Favourite = () => {
-
-    //get API
-    const userStore = useSelector((state) => state.auth.login?.currentUser);
-    const [entireJobs, setEntireJobs] = useState();
-    const [recomendedJobs, setRecomendJobs] = useState();
-    useEffect(() => {
-        if (userStore.role === "JobSeeker") {
-            axios.get(`${config.api.url}/job/all-mine`, { headers: { Authorization: `Bearer ${userStore.accessToken}` } })
-                .then((res) => {
-                    console.log(res.data)
-                    setEntireJobs(res.data);
-                })
-                .catch(err => {
-                    console.log(err)
-                });
-            axios.get(`${config.api.url}/job/recommend/3`, { headers: { Authorization: `Bearer ${userStore.accessToken}` } })
-                .then((res) => {
-                    setRecomendJobs(res.data);
-                })
-                .catch(err => {
-                    console.log(err)
-                });
-        }
-    }, [userStore])
-
-    // convert api to object
-    let jobs = [];
-    if (entireJobs) {
-        jobs = entireJobs.map((job) => {
-            let tags = job.tags.replace(" ", '').split(",")
-            tags = tags.slice(0, 7)
-            return {
-                id: job.id,
-                logo: job.authorAvatar,
-                companyName: job.authorName,
-                location: job.authorAddress,
-                category: job.title,
-                jobType: job.typeOfWorking,
-                experience: job.exp,
-                minSalary: job.salary,
-                maxSalary: job.salary,
-                skills: tags,
-            }
+  //get API
+  const userStore = useSelector((state) => state.auth.login?.currentUser);
+  const [entireJobs, setEntireJobs] = useState();
+  const [recomendedJobs, setRecomendJobs] = useState();
+  useEffect(() => {
+    if (userStore.role === "JobSeeker") {
+      axios
+        .get(`${config.api.url}/job/all-mine`, { headers: { Authorization: `Bearer ${userStore.accessToken}` } })
+        .then((res) => {
+          console.log(res.data);
+          setEntireJobs(res.data);
         })
-    }
-    let rJ = []
-    if (recomendedJobs) {
-        console.log(recomendedJobs)
-        rJ = recomendedJobs.map((job) => {
-            return {
-                id: job.id,
-                companyName: job.authorName,
-                logo: job.authorAvatar,
-                jobs: [{
-                    id: "1",
-                    category: job.title,
-                    jobType: job.typeOfWorking,
-                    location: job.authorAddress,
-                }, {
-                    id: "2",
-                    category: job.title,
-                    jobType: job.typeOfWorking,
-                    location: job.authorAddress,
-            }]
-            }
+        .catch((err) => {
+          console.log(err);
+        });
+      axios
+        .get(`${config.api.url}/job/recommend/3`, { headers: { Authorization: `Bearer ${userStore.accessToken}` } })
+        .then((res) => {
+          setRecomendJobs(res.data);
         })
-        // setRecomendJobs(rJ)
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  }, [userStore]);
 
-    return (
-        <Fragment>
-            <FavouriteJob jobs = {DUMMY_JOB}/>
-            {/* <RecomendedJob /> */}
-        </Fragment>
-    )
-}
+  // convert api to object
+  let jobs = [];
+  if (entireJobs) {
+    jobs = entireJobs.map((job) => {
+      let tags = job.tags.replace(" ", "").split(",");
+      tags = tags.slice(0, 7);
+      return {
+        id: job.id,
+        logo: job.authorAvatar,
+        companyName: job.authorName,
+        location: job.authorAddress,
+        category: job.title,
+        jobType: job.typeOfWorking,
+        experience: job.exp,
+        minSalary: job.salary,
+        maxSalary: job.salary,
+        skills: tags,
+      };
+    });
+  }
+  let rJ = [];
+  if (recomendedJobs) {
+    console.log(recomendedJobs);
+    rJ = recomendedJobs.map((job) => {
+      return {
+        id: job.id,
+        companyName: job.authorName,
+        logo: job.authorAvatar,
+        jobs: [
+          {
+            id: "1",
+            category: job.title,
+            jobType: job.typeOfWorking,
+            location: job.authorAddress,
+          },
+          {
+            id: "2",
+            category: job.title,
+            jobType: job.typeOfWorking,
+            location: job.authorAddress,
+          },
+        ],
+      };
+    });
+    // setRecomendJobs(rJ)
+  }
 
-export default Favourite
+  return (
+    <Fragment>
+      <FavouriteJob jobs={DUMMY_JOB} />
+      {/* <RecomendedJob /> */}
+    </Fragment>
+  );
+};
+
+export default Favourite;
