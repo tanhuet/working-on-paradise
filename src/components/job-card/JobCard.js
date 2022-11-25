3732d9df922500c6923f54536b24e565be3d3ffa
Hiep Do <hiep23022002@gmail.com>
2022-11-26 00:53:37 +0700

    fix bookmark color

--- src/components/job-card/JobCard.js
+++ src/components/job-card/JobCard.js
@@ -51,7 +51,7 @@ const JobCard = (props) => {
         <div className={classes.btn}>
           <button onClick={followHanlder}>
             {!props.bookmark && <FollowIcon />}
-            {props.bookmark && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
+            {props.bookmark && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" fill="#7BEADF" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
               <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
             </svg>}
           </button>
