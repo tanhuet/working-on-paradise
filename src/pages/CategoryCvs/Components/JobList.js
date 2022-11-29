ec042f24913269a352f49cae77403b45ff5a1092
canhung <hungmienhop@gmail.com>
2022-11-30 02:09:26 +0700

    fix button down

--- src/pages/CategoryCvs/Components/JobList.js
+++ src/pages/CategoryCvs/Components/JobList.js
@@ -79,17 +79,15 @@ const JobList = (props) => {
         <div className={classes["page-controller"]}>
           {/* <button onClick={prePage}>Previous</button> */}
           <button onClick={nextPage}>
-            {" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="50"
-              height="50"
-              fill="#6CE0E0"
-              class="bi bi-caret-down-square"
+              width="70"
+              height="70"
+              fill="#48CCCD"
+              class="bi bi-caret-down-fill"
               viewBox="0 0 16 16"
             >
-              <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z" />
-              <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z" />
+              <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
             </svg>
           </button>
         </div>
