e2096b46957e90235110d5faca07d90d35adaa79
Hiep Do <hiep23022002@gmail.com>
2022-12-03 01:01:43 +0700

    responsive: navigation - category

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -66,11 +66,21 @@ const MainNavigation = () => {
               </ul>
             )}
             <form className="d-flex" onSubmit={searchHander}>
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" ref={suggestion}/>
+              <input className={`form-control me-2 ${classes['input']}`} type="search" placeholder="Search" aria-label="Search" ref={suggestion}/>
               {/* <button type="submit"></button> */}
             </form>
             {user ? (
               <ul className={`navbar-nav ${classes.auth}`}>
+                <li className={classes['messager-icon']}>
+                  <NavLink to="/message">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-messenger" viewBox="0 0 16 16">
+                    <path d="M0 7.76C0 3.301 3.493 0 8 0s8 3.301 8 7.76-3.493 7.76-8 7.76c-.81 0-1.586-.107-2.316-.307a.639.639 0 0 0-.427.03l-1.588.702a.64.64 0 0 1-.898-.566l-.044-1.423a.639.639 0 0 0-.215-.456C.956 12.108 0 10.092 0 7.76zm5.546-1.459-2.35 3.728c-.225.358.214.761.551.506l2.525-1.916a.48.48 0 0 1 .578-.002l1.869 1.402a1.2 1.2 0 0 0 1.735-.32l2.35-3.728c.226-.358-.214-.761-.551-.506L9.728 7.381a.48.48 0 0 1-.578.002L7.281 5.98a1.2 1.2 0 0 0-1.735.32z"/>
+                  </svg>
+                  </NavLink>
+                </li>
+                <li className={`nav-item ${classes.messager}`}>
+                  <NavLink className="nav-link" href="#" to="/message">Chat</NavLink>
+                </li>
                 <li className="nav-item">
                 <NavLink className={`nav-link ${classes['btn-logout']}`} href="#" to="/account">{user.name}</NavLink>
                 </li>
