b855be325f00b9ef052c4e62c4ec2e710e8730be
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-12-08 05:36:08 +0700

    fix: Navigate page employer if jobseeker

--- src/pages/account-employer/AccEmp.js
+++ src/pages/account-employer/AccEmp.js
@@ -3,24 +3,25 @@ import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import config from "../../config";
+import { useNavigate } from "react-router-dom";
 
 const AccEmp = () => {
-    const userStore = useSelector((state) => state.auth.login?.currentUser);
+  const userStore = useSelector((state) => state.auth.login?.currentUser);
   const [user, setUser] = useState();
 
+  const navigator = useNavigate();
   useEffect(() => {
     var endPoint = "jobseeker";
     if (userStore.role === "Employer") {
       endPoint = "employer";
+    } else {
+      navigator("/home");
     }
-    axios.get(`${config.api.url}/${endPoint}`, { headers: { Authorization: `Bearer ${userStore.accessToken}` } })
-      .then((res) => {
-        //console.log(res.data);
-        setUser(res.data);
+    axios.get(`${config.api.url}/${endPoint}`, { headers: { Authorization: `Bearer ${userStore.accessToken}` } }).then((res) => {
+      //console.log(res.data);
+      setUser(res.data);
     });
-  }, [userStore]);
-    return (
-        <AccountEmployer user={user} />
-    )
-}
+  });
+  return <AccountEmployer user={user} />;
+};
 export default AccEmp;
